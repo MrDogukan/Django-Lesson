c9966f1259c7b52dd28ab92c8fd20b29db62bfb9
MrDogukan <aliseslikaya@gmail.com>
2022-01-26 21:20:42 +0300

    first commit

---
+++ relationship/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Creator(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.first_name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+    yazar =models.OneToOneField(Creator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Frameworks(models.Model):
+    name = models.CharField(max_length=20)
+    Dil = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Developers(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    frameworks =models.ManyToManyField(Frameworks)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} '
